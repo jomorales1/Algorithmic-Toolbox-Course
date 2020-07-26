9f319abc8c573107d46651d9125fcd52eff53d72
Jorge Morales <manriquem1901@gmail.com>
2020-07-26 18:37:19 -0500

    Week 2 - Problem 2 was solved.

--- course1_2020_05_28/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ course1_2020_05_28/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
+const int size = (int) 1e7 + 1;
+int f[size];
+
+int get_fibonacci_last_digit_fast(int n) {
+    f[0] = 0;
+    f[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        f[i] = (f[i - 1] + f[i - 2]) % 10;
+    }
+    return f[n];
+}
+
 int get_fibonacci_last_digit_naive(int n) {
     if (n <= 1)
         return n;
@@ -19,6 +33,6 @@ int get_fibonacci_last_digit_naive(int n) {
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
+    int c = get_fibonacci_last_digit_fast(n);
     std::cout << c << '\n';
-    }
+}
