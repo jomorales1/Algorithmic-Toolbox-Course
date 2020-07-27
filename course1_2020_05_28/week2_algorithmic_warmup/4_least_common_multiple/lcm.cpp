dc4952e495811320b4ceec56c3cb2e89ad8754a8
Jorge Morales <manriquem1901@gmail.com>
2020-07-27 10:04:10 -0500

    Week 2 - Problem 4 was solved.

--- course1_2020_05_28/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ course1_2020_05_28/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -1,4 +1,35 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<pair<int, int>> factors(int n) {
+  vector<pair<int, int>> f;
+  for (int i = 2; i * i <= n; i++) {
+    if (n % i == 0){
+      pair<int, int> factor;
+      factor.first = i;
+      factor.second = 1;
+      n /= i;
+      while (n % i == 0) {
+        factor.second++;
+        n /= i;
+      }
+      f.push_back(factor);
+    }
+  }
+  if (n > 1) {
+    f.push_back(make_pair(n, 1));
+  }
+  return f;
+}
+
+int gcd_fast(int a, int b) {
+  if (b > a)
+    swap(a, b);
+  if (b == 0)
+    return a;
+  return gcd_fast(b, a % b);
+}
 
 long long lcm_naive(int a, int b) {
   for (long l = 1; l <= (long long) a * b; ++l)
@@ -8,9 +39,19 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+long long lcm_fast(int a, int b) {
+  return (long long) a * b / gcd_fast(a, b);
+}
+
 int main() {
   int a, b;
-  std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  cin >> a >> b;
+  cout << lcm_fast(a, b) << "\n";
+  /*int n;
+  cin >> n;
+  vector<pair<int, int>> fac = factors(n);
+  for (auto x: fac) {
+    cout << x.first << "^" << x.second << "\n";
+  }*/
   return 0;
 }
