f1639392a646e0e217fb97ced9d3c3f7a4bd2bda
Jorge Morales <manriquem1901@gmail.com>
2020-07-27 16:12:37 -0500

    Week 2 - All problems solved!

--- course1_2020_05_28/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
+++ course1_2020_05_28/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
@@ -1,4 +1,47 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fibonacci(long long n, long long m) {
+    int f[n + 1];
+    f[0] = 0;
+    f[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        f[i] = (f[i - 1] + f[i - 2]) % m;
+    }
+    return f[n];
+}
+
+long long get_fibonacci_huge_fast(long long n, long long m) {
+    /*long long f[n + 1];
+    f[0] = 0;
+    f[1] = 1;
+    cout << f[0] << " " << f[1] << " ";
+    for (int i = 2; i <= n; i++){
+        f[i] = (f[i - 1] + f[i - 2]) % m;
+        cout << f[i] << " ";
+    }*/
+    if (n <= 1)
+        return n;
+
+    int p = 0;
+    vector<int> period;
+    period.push_back(0);
+    period.push_back(1);
+    for (int i = 2; i < m * m; i++) {
+        if (fibonacci(i, m) == 0 && fibonacci(i + 1, m) == 1){
+            p = i;
+            break;
+        }
+        period.push_back(fibonacci(i, m));
+    }
+    long long pos = n % p;
+    return period[pos];
+}
+
+int fibonacci_sum_squares_fast(long long n) {
+    return (get_fibonacci_huge_fast(n, 10) * get_fibonacci_huge_fast(n + 1, 10)) % 10;
+}
 
 int fibonacci_sum_squares_naive(long long n) {
     if (n <= 1)
@@ -20,6 +63,7 @@ int fibonacci_sum_squares_naive(long long n) {
 
 int main() {
     long long n = 0;
-    std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+    cin >> n;
+    cout << fibonacci_sum_squares_fast(n) << "\n";
+    return 0;
 }
