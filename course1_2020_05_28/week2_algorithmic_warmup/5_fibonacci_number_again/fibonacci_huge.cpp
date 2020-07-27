f93dcfd90d7c85affba7fd98f3a89839a578e405
Jorge Morales <manriquem1901@gmail.com>
2020-07-27 10:57:48 -0500

    Week 2 - Problem 5 was solved.

--- course1_2020_05_28/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
+++ course1_2020_05_28/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
@@ -1,4 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long get_fibonacci_huge_fast(long long n, long long m) {
+    /*long long f[n + 1];
+    f[0] = 0;
+    f[1] = 1;
+    cout << f[0] << " " << f[1] << " ";
+    for (int i = 2; i <= n; i++){
+        f[i] = (f[i - 1] + f[i - 2]) % m;
+        cout << f[i] << " ";
+    }*/
+    if (n <= 1)
+        return n;
+
+    vector<int> period;
+    long long prev = 0;
+    long long curr = 1;
+    period.push_back(0);
+    while (curr != 0) {
+        period.push_back(curr);
+        long long tmp_prev = prev;
+        prev = curr;
+        curr = (tmp_prev + curr) % m;
+    }
+    long long pos = n % period.size();
+    return period[pos];
+}
 
 long long get_fibonacci_huge_naive(long long n, long long m) {
     if (n <= 1)
@@ -18,6 +46,10 @@ long long get_fibonacci_huge_naive(long long n, long long m) {
 
 int main() {
     long long n, m;
-    std::cin >> n >> m;
-    std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+    cin >> n >> m;
+    cout << get_fibonacci_huge_fast(n, m) << '\n';
+    /*for (int i = 1; i < 4; i++) {
+        get_fibonacci_huge_fast(50, i + 1);
+    }*/
+    return 0;
 }
