91140363e9af3cc3f19bf6242a5ddcd8a9dbfe48
Jorge Morales <manriquem1901@gmail.com>
2020-08-04 14:20:46 -0500

    Week 3 - Solution to the 1st problem was added.

--- course1_2020_05_28/week3_greedy_algorithms/1_money_change/change.cpp
+++ course1_2020_05_28/week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,12 +1,34 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int get_change(int m) {
   //write your code here
+  int n = 0;
+  while (m > 0) {
+    while (m >= 10) {
+      m -= 10;
+      n++;
+      // cout << 10 << " ";
+    }
+    while (m >= 5) {
+      m -= 5;
+      n++;
+      // cout << 5 << " ";
+    }
+    while (m >= 1) {
+      m -= 1;
+      n++;
+      // cout << 1 << " ";
+    }
+  }
+  // cout << "\n";
   return n;
 }
 
 int main() {
   int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+  cin >> m;
+  cout << get_change(m) << '\n';
+  return 0;
 }
