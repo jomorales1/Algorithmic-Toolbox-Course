2a8ade4d98f63ce37aa08b78b83b77188591eba9
Jorge Morales <manriquem1901@gmail.com>
2020-08-04 16:53:43 -0500

    Week 3 - Solution for the 4th problem was added.

--- course1_2020_05_28/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
+++ course1_2020_05_28/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
@@ -1,27 +1,28 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
-using std::vector;
+using namespace std;
 
 long long max_dot_product(vector<int> a, vector<int> b) {
   // write your code here
   long long result = 0;
-  for (size_t i = 0; i < a.size(); i++) {
+  for (int i = 0; i < a.size(); i++) {
     result += ((long long) a[i]) * b[i];
   }
   return result;
 }
 
 int main() {
-  size_t n;
-  std::cin >> n;
+  int n;
+  cin >> n;
   vector<int> a(n), b(n);
-  for (size_t i = 0; i < n; i++) {
-    std::cin >> a[i];
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
   }
-  for (size_t i = 0; i < n; i++) {
-    std::cin >> b[i];
+  for (int i = 0; i < n; i++) {
+    cin >> b[i];
   }
-  std::cout << max_dot_product(a, b) << std::endl;
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+  cout << max_dot_product(a, b) << "\n";
+  return 0;
 }
