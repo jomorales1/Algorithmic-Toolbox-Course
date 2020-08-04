bfcdf83f4a6a492a4b20bffc925f4ad4f46fcabc
Jorge Morales <manriquem1901@gmail.com>
2020-08-04 16:07:19 -0500

    Week 3 - Solution to the 3rd problem was added.

--- course1_2020_05_28/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ course1_2020_05_28/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -1,14 +1,25 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
-using std::cin;
-using std::cout;
-using std::vector;
-using std::max;
+using namespace std;
 
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
     // write your code here
-    return -1;
+    int n = stops.size();
+    int num_refills = 0;
+    int current_refill = 0;
+    while (current_refill < n) {
+        int last_refill = current_refill;
+        while (current_refill < n && stops[current_refill + 1] - stops[last_refill] <= tank) {
+            current_refill++;
+        }
+        if (current_refill == last_refill) {
+            return -1;
+        }
+        if (dist - stops[current_refill] > 0) {
+            num_refills++;
+        }
+    }
+    return num_refills;
 }
 
 
@@ -21,7 +32,7 @@ int main() {
     cin >> n;
 
     vector<int> stops(n);
-    for (size_t i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
         cin >> stops.at(i);
 
     cout << compute_min_refills(d, m, stops) << "\n";
