f3db8e1b9b380313afbf11a1104487421b980475
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-08-11 13:38:12 -0500

    Recursive binary search was added, also the iterative function was fixed.

--- Week 4/Binary Search/binary_search.cpp
+++ Week 4/Binary Search/binary_search.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int binary_search(const vector<int> &numbers, int a) {
     int low = 0;
-    int high = numbers.size();
+    int high = numbers.size() - 1;
     int k;
     while (low <= high) {
         k = (low + high) / 2;
@@ -18,6 +18,18 @@ int binary_search(const vector<int> &numbers, int a) {
     return -1;
 }
 
+int recursive_binary_search(const vector<int> &numbers, int low, int high, int a) {
+    if (high < low)
+        return low - 1;
+    int mid = (low + high) / 2;
+    if (numbers[mid] == a)
+        return mid;
+    else if (numbers[mid] > a)
+        return recursive_binary_search(numbers, low, mid - 1, a);
+    else
+        return recursive_binary_search(numbers, mid + 1, high, a);
+}
+
 int main() {
     // TODO: implement binary search
     int n = 0;
@@ -27,8 +39,15 @@ int main() {
         cin >> numbers[i];
     }
     sort(numbers.begin(), numbers.end());
+    cout << "Sorted array: \n";
+    for (auto number : numbers) {
+        cout << number << " ";
+    }
+    cout << '\n';
     int obj = 0;
     cin >> obj;
-    int index = binary_search(numbers, obj);
-    cout << index << '\n';
+    int index1 = binary_search(numbers, obj);
+    int index2 = recursive_binary_search(numbers, 0, n - 1, obj);
+    cout << "Iterative: " << index1 << '\n';
+    cout << "Recursive: " << index2 << '\n';
 }
