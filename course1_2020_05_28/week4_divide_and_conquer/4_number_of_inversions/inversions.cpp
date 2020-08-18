1c6345c76a4cade4c60b746a59bca155ebba5b9e
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-08-18 14:53:24 -0500

    Week 4 - Fourth problem solved!

--- course1_2020_05_28/week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
+++ course1_2020_05_28/week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
@@ -1,25 +1,109 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+#include <cstdlib>
 
-using std::vector;
+using namespace std;
 
-long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
-  long long number_of_inversions = 0;
-  if (right <= left + 1) return number_of_inversions;
-  size_t ave = left + (right - left) / 2;
-  number_of_inversions += get_number_of_inversions(a, b, left, ave);
-  number_of_inversions += get_number_of_inversions(a, b, ave, right);
-  //write your code here
-  return number_of_inversions;
+void print_vector(const vector<int> &elements) {
+    for (auto element : elements)
+        cout << element << " ";
+    cout << '\n';
+}
+
+vector<int> subarr(vector<int> &A, int left, int right) {
+    vector<int> res;
+    for (int i = left; i <= right; i++) {
+        res.push_back(A[i]);
+    }
+    return res;
+}
+
+vector<int> merge(vector<int> &B, vector<int> &C) {
+    int index_b = 0;
+    int index_c = 0;
+    vector<int> D;
+    while(index_b < B.size() && index_c < B.size()) {
+        int b = B[index_b];
+        int c = C[index_c];
+        if (b <= c) {
+            D.push_back(b);
+            index_b++;
+        } else {
+            D.push_back(c);
+            index_c++;
+        }
+    }
+    while (index_b < B.size()) {
+        int b = B[index_b];
+        D.push_back(b);
+        index_b++;
+    }
+    while (index_c < C.size()) {
+        int c = C[index_c];
+        D.push_back(c);
+        index_c++;
+    }
+    return D;
+}
+
+int merge_sort(vector<int> &A) {
+    if (A.size() == 1)
+        return 0;
+    int m = (A.size() - 1) / 2;
+    vector<int> b = subarr(A, 0, m);
+    vector<int> c = subarr(A, m + 1, A.size() - 1);
+    int res = 0;
+    for (int i = 0; i < b.size(); i++) {
+        for (int j = 0; j < c.size(); j++) {
+            if (b[i] > c[j])
+                res++;
+        }
+    }
+    return res + merge_sort(b) + merge_sort(c);
+}
+
+int naive_solution(const vector<int> &A) {
+    int res = 0;
+    int n = A.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (A[i] > A[j])
+                res++;
+        }
+    }
+    return res;
+}
+
+void stress_test() {
+    while(true) {
+        int n = rand() % 10 + 6;
+        vector<int> numbers(n);
+        int element = -1;
+        for (int i = 0; i < n; i++) {
+            element = rand() % 10 + 1;
+            numbers[i] = element;
+            cout << element << " ";
+        }
+        cout << '\n';
+        int res1 = naive_solution(numbers);
+        int res2 = merge_sort(numbers);
+        if (res1 != res2) {
+            cout << "Wrong answer: " << res1 << " " << res2 << '\n';
+            break;
+        } else {
+            cout << "0K\n";
+        }
+    }
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
-    std::cin >> a[i];
-  }
-  vector<int> b(a.size());
-  std::cout << get_number_of_inversions(a, b, 0, a.size()) << '\n';
+    int n = 0;
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+    int res = merge_sort(numbers);
+    cout << res << '\n';
+    //stress_test();
+    return 0;
 }
