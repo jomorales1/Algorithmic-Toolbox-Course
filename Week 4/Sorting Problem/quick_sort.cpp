85f9a9cb75788f34fa65b505dc977175e99636b7
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-08-17 19:23:04 -0500

    A bug in the three way partition was fixed. Tile recursion implementation was added in order to reduce space complexity of the quick sort algorithm.

--- Week 4/Sorting Problem/quick_sort.cpp
+++ Week 4/Sorting Problem/quick_sort.cpp
@@ -21,15 +21,19 @@ pair<int, int> partition2(int numbers[], int left, int right) {
     int j = left;
     int k = left;
     for (int i = left + 1; i <= right; i++) {
-        if (numbers[i] <= x) {
+        if (numbers[i] == x) {
+            k++;
+            swap(numbers[k], numbers[i]);
+        }
+    }
+    for (int i = k; i <= right; i++) {
+        if (numbers[i] < x) {
             j++;
             swap(numbers[j], numbers[i]);
         }
-        if (numbers[i] == x)
-            k++;
     }
     swap(numbers[left], numbers[j]);
-    pair<int, int> res = {k, j};
+    pair<int, int> res = {left, j};
     return res;
 }
 
@@ -47,8 +51,8 @@ void randomized_quick_sort(int numbers[], int left, int right) {
     int k = rand() % (right - left + 1) + left;
     swap(numbers[left], numbers[k]);
     int m = partition(numbers, left, right);
-    quick_sort(numbers, left, m - 1);
-    quick_sort(numbers, m + 1, right);
+    randomized_quick_sort(numbers, left, m - 1);
+    randomized_quick_sort(numbers, m + 1, right);
 }
 
 void randomized_quick_sort2(int numbers[], int left, int right) {
@@ -59,8 +63,18 @@ void randomized_quick_sort2(int numbers[], int left, int right) {
     pair<int, int> m = partition2(numbers, left, right);
     int m1 = m.first;
     int m2 = m.second;
-    quick_sort(numbers, left, m1 - 1);
-    quick_sort(numbers, m2 + 1, right);
+    randomized_quick_sort2(numbers, left, m1 - 1);
+    randomized_quick_sort2(numbers, m2 + 1, right);
+}
+
+void randomized_quick_sort3(int numbers[], int left, int right) {
+    while (left < right) {
+        int k = rand() % (right - left + 1) + left;
+        swap(numbers[left], numbers[k]);
+        int m = partition(numbers, left, right);
+        randomized_quick_sort3(numbers, left, m - 1);
+        left = m + 1;
+    }
 }
 
 int main() {
