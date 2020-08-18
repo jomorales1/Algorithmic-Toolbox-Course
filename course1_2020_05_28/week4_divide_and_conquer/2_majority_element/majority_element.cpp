a266615b1cc52e2dfa2af370b4508e54617bfacf
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-08-18 11:20:58 -0500

    Week 4 - Second problem solved!

--- course1_2020_05_28/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ course1_2020_05_28/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -1,22 +1,38 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
-using std::vector;
+using namespace std;
 
 int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //write your code here
-  return -1;
+    if (left == right) return -1;
+    if (left + 1 == right) return a[left];
+    //write your code here
+    int m = (left + right) / 2;
+    int a1 = get_majority_element(a, left, m);
+    int a2 = get_majority_element(a, m, right);
+    if (a1 == a2) {
+        return a1;
+    }
+    int count1 = 0, count2 = 0;
+    for (int i = left; i < right; i++) {
+        if (a[i] == a1)
+            count1++;
+        if (a[i] == a2)
+            count2++;
+    }
+    if (count1 > count2 && count1 > (right - left) / 2)
+        return a1;
+    if (count2 > count1 && count2 > (right - left) / 2)
+        return a2;
+    return -1;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cin >> a[i];
-  }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+    int n = 0;
+    cin >> n;
+    vector<int> a(n);
+    for (size_t i = 0; i < a.size(); ++i) {
+        cin >> a[i];
+    }
+    cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+    return 0;
 }
