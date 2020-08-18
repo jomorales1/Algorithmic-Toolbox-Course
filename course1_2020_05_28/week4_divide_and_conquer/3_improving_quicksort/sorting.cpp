c85e0b804dd2c07d314bdfa1127da580eada74af
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-08-18 14:06:45 -0500

    A bus in the quick sort algorithm with three way partition was fixed.

--- course1_2020_05_28/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ course1_2020_05_28/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -35,7 +35,27 @@ pair<int, int> partition(vector<int> &a, int left, int right) {
             aux++;
         }
     }
-    return {aux, k};    
+    return {aux, k};
+}
+
+pair<int, int> partition3(vector<int> &a, int left, int right) {
+    int x = a[left];
+    int j = left;
+    int aux = left - 1;
+    for (int i = left + 1; i <= right; i++) {
+        if (a[i] == x) {
+            j++;
+            swap(a[i], a[j]);
+        } else if(a[i] < x) {
+            j++;
+            aux++;
+            swap(a[i], a[j]);
+            swap(a[j], a[aux]);
+        }
+    }
+    if (aux < left)
+        aux = left;
+    return {aux, j};
 }
 
 void randomized_quick_sort(vector<int> &a, int l, int r) {
@@ -45,11 +65,11 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
 
     int k = l + rand() % (r - l + 1);
     swap(a[l], a[k]);
-    pair<int, int> m = partition(a, l, r);
+    pair<int, int> m = partition3(a, l, r);
     int m1 = m.first;
     int m2 = m.second;
 
-    randomized_quick_sort(a, l, m1 - 1);
+    randomized_quick_sort(a, l, m1);
     randomized_quick_sort(a, m2 + 1, r);
 }
 
@@ -99,6 +119,7 @@ void stress_test() {
 }
 
 int main() {
+    //stress_test();
     int n = 0;
     cin >> n;
     vector<int> a(n);
