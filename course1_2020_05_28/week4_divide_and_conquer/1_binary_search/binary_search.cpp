e922409716e98f770f3062bb7b507c353ac17adc
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-08-18 13:35:09 -0500

    A bug in the implementation of the binary search was fixed.

--- course1_2020_05_28/week4_divide_and_conquer/1_binary_search/binary_search.cpp
+++ course1_2020_05_28/week4_divide_and_conquer/1_binary_search/binary_search.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
+#include <cstdlib>
 
 using namespace std;
 
 int binary_search(const vector<int> &a, int x) {
-    int left = 0, right = (int)a.size(); 
+    int left = 0;
+    int right = a.size() - 1; 
     //write your code here
     while (left <= right) {
         int k = (left + right) / 2;
@@ -24,6 +26,46 @@ int linear_search(const vector<int> &a, int x) {
     return -1;
 }
 
+void stress_test() {
+    while(true) {
+        int n = rand() % 100 + 100;
+        vector<int> numbers(n);
+        int element = 0;
+        for (int i = 0; i < n; i++) {
+            element = rand() % 100 + 1;
+            numbers[i] = element;
+        }
+        sort(numbers.begin(), numbers.end());
+        int k = rand() % 100 + 10;
+        vector<int> s(k);
+        for (int i = 0; i < k; i++) {
+            element = rand() % 100 + 1;
+            s[i] = element;
+        }
+        int res1 = -1;
+        int res2 = -1;
+        bool diff = true;
+        for (int i = 0; i < k; i++) {
+            res1 = binary_search(numbers, s[i]);
+            res2 = linear_search(numbers, s[i]);
+            diff = res1 == res2;
+            if (!diff) {
+                for (int j = 0; j < n; j++) {
+                    cout << numbers[j] << " ";
+                }
+                cout << '\n';
+                cout << "Wrong answer\n";
+                cout << "Element: " << s[i] << '\n';
+                cout << "BS: " << res1 << ", " << "LS: " << res2 << '\n';
+                break;
+            }
+            cout << "OK\n";
+        }
+        if (!diff)
+            break;
+    }
+}
+
 int main() {
     int n = 0;
     cin >> n;
@@ -42,5 +84,6 @@ int main() {
         cout << binary_search(a, b[i]) << ' ';
     }
     cout << '\n';
+    //stress_test();
     return 0;
 }
