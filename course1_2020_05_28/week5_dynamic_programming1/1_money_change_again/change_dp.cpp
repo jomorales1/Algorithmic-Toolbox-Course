dd4867034cb2f7b20817ba2de29e2397569bed62
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-08-23 21:39:13 -0500

    Week 5 - First problem solved!

--- course1_2020_05_28/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ course1_2020_05_28/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,12 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-int get_change(int m) {
-  //write your code here
-  return m / 4;
+using namespace std;
+
+const int size = (int) 1e6 + 1;
+int dp[size];
+
+int dp_change(vector<int> &coins, int amount) {
+    dp[0] = 0;
+    for (int m = 1; m <= amount; m++) {
+        dp[m] = INT_MAX;
+        for (int i = 0; i < coins.size(); i++) {
+            if (m >= coins[i]) {
+                int num_coins = dp[m - coins[i]] + 1;
+                if (num_coins < dp[m]) {
+                    dp[m] = num_coins;
+                }
+            }
+        }
+    }
+    return dp[amount];
 }
 
 int main() {
-  int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+    vector<int> coins = {1, 3, 4};
+    int amount = 0;
+    cin >> amount;
+    int result = dp_change(coins, amount);
+    cout << result << '\n';
+    return 0;
 }
