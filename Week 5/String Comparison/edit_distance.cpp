ce8260ea1f9a06690482f54a30c50c86bacf8152
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-08-23 20:01:10 -0500

    Output alignment was added.

--- Week 5/String Comparison/edit_distance.cpp
+++ Week 5/String Comparison/edit_distance.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
+const int s = (int) 1e3 + 1;
+int dp[s][s];
+
 int edit_distance(string a, string b) {
     int n = a.length();
     int m = b.length();
-    int dp[n + 1][m + 1];
     for (int i = 0; i <= n; i++) {
         dp[i][0] = i;
     }
@@ -27,9 +29,26 @@ int edit_distance(string a, string b) {
     return dp[n][m];
 }
 
+void output_alignment(string a, string b, int i, int j) {
+    if (i == -1 && j == -1)
+        return;
+    if (i >= 0 && dp[i][j] == dp[i - 1][j] + 1) {
+        output_alignment(a, b, i - 1, j);
+        cout << a[i] << ' ' << '-' << '\n';
+    }
+    else if (j >= 0 && dp[i][j] == dp[i][j - 1] + 1) {
+        output_alignment(a, b, i, j - 1);
+        cout << '-' << ' ' << b[j] << '\n';
+    } else {
+        output_alignment(a, b, i - 1, j - 1);
+        cout << a[i] << ' ' << b[j] << '\n';
+    }
+}
+
 int main() {
     string a, b;
     cin >> a >> b;
     cout << "Edit distance: " << edit_distance(a, b) << '\n';
+    output_alignment(a, b, a.length(), b.length());
     return 0;
 }
