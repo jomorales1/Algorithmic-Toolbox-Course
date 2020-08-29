d60c1f00c8e9c2a23b9abf4ae253342d784d314c
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-08-29 15:42:38 -0500

    Week 6 - First problem was solved!

--- course1_2020_05_28/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ course1_2020_05_28/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -1,25 +1,53 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
-using std::vector;
+using namespace std;
 
 int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+    //write your code here
+    int current_weight = 0;
+    for (int i = 0; i < w.size(); ++i) {
+        if (current_weight + w[i] <= W) {
+        current_weight += w[i];
+        }
     }
-  }
-  return current_weight;
+    return current_weight;
+}
+
+int knapsack(vector<int> &weights, vector<int> &prizes, int W) {
+    int n = weights.size();
+    vector<vector<int>> value(W + 1, vector<int> (n + 1));
+    for (int i = 0; i < n + 1; i++) {
+        value[0][i] = 0;
+    }
+    for (int i = 0; i < W + 1; i++) {
+        value[i][0] = 0;
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int w = 1; w <= W; w++) {
+            value[w][i] = value[w][i - 1];
+            if (weights[i - 1] <= w) {
+                int val = value[w - weights[i - 1]][i - 1] + prizes[i - 1];
+                if (value[w][i] < val) {
+                    value[w][i] = val;
+                }
+            }
+        }
+    }
+    return value[W][n];
 }
 
 int main() {
-  int n, W;
-  std::cin >> W >> n;
-  vector<int> w(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> w[i];
-  }
-  std::cout << optimal_weight(W, w) << '\n';
+    int n, W;
+    cin >> W >> n;
+    vector<int> weights(n);
+    vector<int> prizes(n);
+    for (int i = 0; i < n; i++) {
+        cin >> weights[i];
+        prizes[i] = weights[i];
+    }
+    sort(weights.begin(), weights.end());
+    sort(prizes.begin(), prizes.end());
+    int result = knapsack(weights, prizes, W);
+    cout << result << '\n';
+    return 0;
 }
