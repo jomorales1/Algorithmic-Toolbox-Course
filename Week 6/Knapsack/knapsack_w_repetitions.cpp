109b0bda0d36d4f8a36c5dc98ebfbbf027ea07f3
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-08-29 13:58:23 -0500

    Recursive solution to the knapsack problem with repetitions was added

--- Week 6/Knapsack/knapsack_w_repetitions.cpp
+++ Week 6/Knapsack/knapsack_w_repetitions.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+const int s = (int) 1e3;
+int dp[s + 1];
+
 int knapsack(vector<int> &weights, vector<int> &prices, int W) {
     int n = weights.size();
     vector<int> value(W + 1);
@@ -20,16 +23,37 @@ int knapsack(vector<int> &weights, vector<int> &prices, int W) {
     return value[W];
 }
 
+int recursive_knapsack(vector<int> &weights, vector<int> &prizes, int w) {
+    if (dp[w] != -1)
+        return dp[w];
+    dp[w] = 0;
+    for (int i = 0; i < weights.size(); i++) {
+        if (weights[i] <= w) {
+            int val = recursive_knapsack(weights, prizes, w - weights[i]) + prizes[i];
+            if (val > dp[w]) {
+                dp[w] = val;
+            }
+        }
+    }
+    return dp[w];
+}
+
 int main() {
     int n, W;
     cin >> n >> W;
     vector<int> weights(n);
-    vector<int> prices(n);
+    vector<int> prizes(n);
     for (int i = 0; i < n; i++) {
         cin >> weights[i];
-        cin >> prices[i];
+        cin >> prizes[i];
     }
-    int result = knapsack(weights, prices, W);
+    dp[0] = 0;
+    for (int i = 1; i <= W; i++) {
+        dp[i] = -1;
+    }
+    int result = knapsack(weights, prizes, W);
+    int result2 = recursive_knapsack(weights, prizes, W);
     cout << result << '\n';
+    cout << result2 << '\n';
     return  0;
 }
